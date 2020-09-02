0207969c8c59136a9a39bc89a54e7827edaab1dc
Alex Shtilman <gungam@outlook.com>
2020-09-02 17:33:32 +0300

    update

--- config.py
+++ config.py
@@ -2,30 +2,32 @@
 import os
 from os import environ, path
 
+
 class Config:
     """Base config."""
-    API_VERSION = '0.0.1'
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-    SETUP_KEY = os.environ.get('SETUP_KEY')
-    STATIC_FOLDER = 'static'
-    TEMPLATES_FOLDER = 'templates'
-    BASE_DIR = '/var/www/connector/'
+
+    API_VERSION = "0.0.1"
+    JWT_SECRET_KEY = open("/run/secrets/jwt_secret_key", "r").read().strip()
+    SETUP_KEY = open("/run/secrets/setup_key", "r").read().strip()
+    STATIC_FOLDER = "static"
+    TEMPLATES_FOLDER = "templates"
+    BASE_DIR = "/var/www/connector/"
     JWT_BLACKLIST_ENABLED = True
-    JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
+    JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-    POSTGRES_USER = os.environ.get('POSTGRES_USER')
-    POSTGRES_DB = os.environ.get('POSTGRES_DB')
-    DB_SERVER_NAME = os.environ.get('DB_SERVER_NAME')
-    DB_SERVER_PORT = os.environ.get('DB_SERVER_PORT', default="5432")
-    SQLALCHEMY_DATABASE_URI = "postgresql://{0}:{1}@{2}:{3}/{4}".format(POSTGRES_USER, POSTGRES_PASSWORD,
-                                                                        DB_SERVER_NAME, DB_SERVER_PORT, POSTGRES_DB)
-    DEFAULT_USER_NAME = os.environ.get('DEFAULT_USER_NAME',default='user')
-    DEFAULT_USER_PASSWD = os.environ.get('DEFAULT_USER_PASSWD',default='user')
+    POSTGRES_PASSWORD = open("/run/secrets/psql_password", "r").read().strip()
+    POSTGRES_USER = open("/run/secrets/psql_user", "r").read().strip()
+    POSTGRES_DB = open("/run/secrets/psql_db", "r").read().strip()
+    DB_SERVER_NAME = "db"
+    DB_SERVER_PORT = "5432"
+    SQLALCHEMY_DATABASE_URI = "postgresql://{0}:{1}@{2}:{3}/{4}".format(
+        POSTGRES_USER, POSTGRES_PASSWORD, DB_SERVER_NAME, DB_SERVER_PORT, POSTGRES_DB
+    )
+    DEFAULT_USER_NAME = os.environ.get("DEFAULT_USER_NAME", default="user")
+    DEFAULT_USER_PASSWD = os.environ.get("DEFAULT_USER_PASSWD", default="user")
 
-    DEFAULT_ADMIN_NAME = os.environ.get('DEFAULT_USER_NAME',default='admin')
-    DEFAULT_ADMIN_PASSWD = os.environ.get('DEFAULT_USER_PASSWD',default='admin')
-
+    DEFAULT_ADMIN_NAME = os.environ.get("DEFAULT_USER_NAME", default="admin")
+    DEFAULT_ADMIN_PASSWD = os.environ.get("DEFAULT_USER_PASSWD", default="admin")
 
 
 class ProdConfig(Config):
@@ -36,6 +38,3 @@ class ProdConfig(Config):
 class DevConfig(Config):
     DEBUG = True
     TESTING = True
-
-
-
