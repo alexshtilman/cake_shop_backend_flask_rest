f90ef6f0b6b429ebd17a8873a63edfb27d9f899e
Alex Shtilman <gungam@outlook.com>
2020-09-07 12:15:12 +0300

    add oauth cred

--- config.py
+++ config.py
@@ -6,15 +6,28 @@ from os import environ, path
 class Config:
     """Base config."""
 
-    API_VERSION = "0.0.1"
-    JWT_SECRET_KEY = open("/run/secrets/jwt_secret_key", "r").read().strip()
-    SETUP_KEY = open("/run/secrets/setup_key", "r").read().strip()
+    API_VERSION = "1.0.0"
     STATIC_FOLDER = "static"
     TEMPLATES_FOLDER = "templates"
-    BASE_DIR = "/var/www/connector/"
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEFAULT_USER_NAME = os.environ.get("DEFAULT_USER_NAME", default="user")
+    DEFAULT_USER_PASSWD = os.environ.get("DEFAULT_USER_PASSWD", default="user")
+    DEFAULT_ADMIN_NAME = os.environ.get("DEFAULT_USER_NAME", default="admin")
+    DEFAULT_ADMIN_PASSWD = os.environ.get("DEFAULT_USER_PASSWD", default="admin")
+    GOOGLE_DISCOVERY_URL = (
+        "https://accounts.google.com/.well-known/openid-configuration"
+    )
+
+
+class ProdConfig(Config):
+    DEBUG = False
+    TESTING = False
+    GOOGLE_CLIENT_ID = open("/run/secrets/google_client_id", "r").read().strip()
+    GOOGLE_CLIENT_SECRET = open("/run/secrets/google_client_secret", "r").read().strip()
+    JWT_SECRET_KEY = open("/run/secrets/jwt_secret_key", "r").read().strip()
+    SETUP_KEY = open("/run/secrets/setup_key", "r").read().strip()
     POSTGRES_PASSWORD = open("/run/secrets/psql_password", "r").read().strip()
     POSTGRES_USER = open("/run/secrets/psql_user", "r").read().strip()
     POSTGRES_DB = open("/run/secrets/psql_db", "r").read().strip()
@@ -23,18 +36,20 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "postgresql://{0}:{1}@{2}:{3}/{4}".format(
         POSTGRES_USER, POSTGRES_PASSWORD, DB_SERVER_NAME, DB_SERVER_PORT, POSTGRES_DB
     )
-    DEFAULT_USER_NAME = os.environ.get("DEFAULT_USER_NAME", default="user")
-    DEFAULT_USER_PASSWD = os.environ.get("DEFAULT_USER_PASSWD", default="user")
-
-    DEFAULT_ADMIN_NAME = os.environ.get("DEFAULT_USER_NAME", default="admin")
-    DEFAULT_ADMIN_PASSWD = os.environ.get("DEFAULT_USER_PASSWD", default="admin")
-
-
-class ProdConfig(Config):
-    DEBUG = False
-    TESTING = False
 
 
 class DevConfig(Config):
     DEBUG = True
     TESTING = True
+    JWT_SECRET_KEY = "My_Secret_key"
+    SETUP_KEY = "my_setup_key"
+    POSTGRES_PASSWORD = "dev_password"
+    POSTGRES_USER = "dev_user"
+    POSTGRES_DB = "127.0.0.1"
+    DB_SERVER_NAME = "db"
+    DB_SERVER_PORT = "5432"
+    SQLALCHEMY_DATABASE_URI = "postgresql://{0}:{1}@{2}:{3}/{4}".format(
+        POSTGRES_USER, POSTGRES_PASSWORD, DB_SERVER_NAME, DB_SERVER_PORT, POSTGRES_DB
+    )
+    GOOGLE_CLIENT_ID = open("/run/secrets/google_client_id", "r").read().strip()
+    GOOGLE_CLIENT_SECRET = open("/run/secrets/google_client_secret", "r").read().strip()
